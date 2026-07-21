3388159686fafd1f6fcc9bf0f53f84cc9e0c5567
delta874 <gabrielcrosby63@gmail.com>
2026-07-21 00:52:43 -0400

    Refactor buildItem function to use orders array

--- script.js
+++ script.js
@@ -1,31 +1,152 @@
 let money = 0;
 
-function buildItem(){
+let selectedParts = [];
 
-    let lens =
-    document.getElementById("lens").checked;
 
-    let processor =
-    document.getElementById("processor").checked;
+let orders = [
 
-    let power =
-    document.getElementById("power").checked;
+{
+name:"Cybernetic Eye",
+parts:["Lens","Processor","Power Cell"],
+reward:100
+},
 
-    if(lens && processor && power){
+{
+name:"Neural Headset",
+parts:["Processor","Memory Chip","Power Cell"],
+reward:150
+},
 
-        money += 100;
+{
+name:"Drone Core",
+parts:["Processor","Gyroscope","Battery"],
+reward:200
+}
 
-        document.getElementById("money").innerText = money;
+];
 
-        document.getElementById("message").innerText =
-        "DELTA: Assembly successful. Payment received.";
 
-    }
-    else{
+let currentOrder = 0;
 
-        document.getElementById("message").innerText =
-        "DELTA: Assembly failed. Check components.";
 
-    }
+function loadOrder(){
+
+let order = orders[currentOrder];
+
+
+document.getElementById("orderName").innerText =
+order.name;
+
+
+let recipe = document.getElementById("recipe");
+
+recipe.innerHTML="";
+
+
+order.parts.forEach(part=>{
+
+let item=document.createElement("li");
+
+item.innerText=part;
+
+recipe.appendChild(item);
+
+});
+
+
+selectedParts=[];
+
+updateSelected();
+
 
 }
+
+
+function selectPart(part){
+
+selectedParts.push(part);
+
+updateSelected();
+
+}
+
+
+function updateSelected(){
+
+let list=document.getElementById("selected");
+
+list.innerHTML="";
+
+
+selectedParts.forEach(part=>{
+
+let item=document.createElement("li");
+
+item.innerText=part;
+
+list.appendChild(item);
+
+});
+
+
+}
+
+
+function build(){
+
+let order=orders[currentOrder];
+
+
+let correct =
+JSON.stringify(selectedParts.sort()) ===
+JSON.stringify(order.parts.sort());
+
+
+if(correct){
+
+money += order.reward;
+
+
+document.getElementById("money").innerText=money;
+
+
+document.getElementById("delta").innerText=
+"DELTA: Assembly successful. Payment received.";
+
+}
+
+else{
+
+
+document.getElementById("delta").innerText=
+"DELTA: Incorrect components detected.";
+
+}
+
+
+}
+
+
+function nextOrder(){
+
+currentOrder++;
+
+
+if(currentOrder >= orders.length){
+
+currentOrder=0;
+
+}
+
+
+document.getElementById("delta").innerText=
+"DELTA: New order received.";
+
+
+loadOrder();
+
+
+}
+
+
+loadOrder();

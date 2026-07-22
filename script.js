709e744bfb5724e0aeb0d23d03ceecff5cbd3e1d
delta874 <gabrielcrosby63@gmail.com>
2026-07-22 00:22:58 -0400

    Add functions for emergency broadcast and security checks

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-console.log("Delta Cybernetics Lab Online");
+function watchTV() {
+  document.getElementById("message").innerHTML =
+  "EMERGENCY BROADCAST: Unknown individuals reported nearby...";
+}
 
+function checkCameras() {
+  document.getElementById("message").innerHTML =
+  "SECURITY CAMERAS ONLINE... Searching area...";
+}
+
+function lookWindow() {
+  document.getElementById("message").innerHTML =
+  "You look outside. The street is empty...";
+}
+
+function checkDoor() {
+  document.getElementById("message").innerHTML =
+  "Door locked.";
+}

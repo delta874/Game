6e85ec59f569bf76e38b90991163e3781c546851
delta874 <gabrielcrosby63@gmail.com>
2026-07-22 00:43:39 -0400

    Refine emergency messages and descriptions

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 function watchTV() {
 
     document.getElementById("message").innerHTML =
-    "EMERGENCY BROADCAST: The Seekers have been reported nearby...";
+    "EMERGENCY BROADCAST: The Seekers have been reported nearby.";
+
 }
 
 
 function checkCameras() {
 
     document.getElementById("message").innerHTML =
-    "SECURITY SYSTEM ONLINE... Checking cameras...";
+    "SECURITY SYSTEM ONLINE... Cameras scanning area.";
+
 }
 
 
 function lookWindow() {
 
     document.getElementById("message").innerHTML =
-    "You look outside. The street is silent...";
+    "You look outside. The street is empty.";
+
 }
 
 
 function checkDoor() {
 
     document.getElementById("message").innerHTML =
-    "Door locked.";
+    "Door locked. You hear nothing.";
+
 }

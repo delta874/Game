bbaa1156b67c0976350a11f205f9da2bf3fb75f1
delta874 <gabrielcrosby63@gmail.com>
2026-07-22 00:48:30 -0400

    Replace message updates with alert prompts

--- script.js
+++ script.js
@@ -1,30 +1,17 @@
+alert("JavaScript is working!");
+
 function watchTV() {
-
-    document.getElementById("message").innerHTML =
-    "EMERGENCY BROADCAST: The Seekers have been reported nearby.";
-
+    alert("TV clicked!");
 }
-
 
 function checkCameras() {
-
-    document.getElementById("message").innerHTML =
-    "SECURITY SYSTEM ONLINE... Cameras scanning area.";
-
+    alert("Security clicked!");
 }
-
 
 function lookWindow() {
-
-    document.getElementById("message").innerHTML =
-    "You look outside. The street is empty.";
-
+    alert("Window clicked!");
 }
 
-
 function checkDoor() {
-
-    document.getElementById("message").innerHTML =
-    "Door locked. You hear nothing.";
-
+    alert("Door clicked!");
 }

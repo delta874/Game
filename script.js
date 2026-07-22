f5a8adcf28fbc5d0e340db5ec52107671832cdc0
delta874 <gabrielcrosby63@gmail.com>
2026-07-22 00:34:21 -0400

    Update emergency messages in script.js

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 function watchTV() {
-  document.getElementById("message").innerHTML =
-  "EMERGENCY BROADCAST: Unknown individuals reported nearby...";
+
+    document.getElementById("message").innerHTML =
+    "EMERGENCY BROADCAST: The Seekers have been reported nearby...";
 }
+
 
 function checkCameras() {
-  document.getElementById("message").innerHTML =
-  "SECURITY CAMERAS ONLINE... Searching area...";
+
+    document.getElementById("message").innerHTML =
+    "SECURITY SYSTEM ONLINE... Checking cameras...";
 }
+
 
 function lookWindow() {
-  document.getElementById("message").innerHTML =
-  "You look outside. The street is empty...";
+
+    document.getElementById("message").innerHTML =
+    "You look outside. The street is silent...";
 }
 
+
 function checkDoor() {
-  document.getElementById("message").innerHTML =
-  "Door locked.";
+
+    document.getElementById("message").innerHTML =
+    "Door locked.";
 }
